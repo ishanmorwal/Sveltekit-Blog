8a0a3c48f905bcbc2b729aa62f5168d9fa837cea
ishan Morwal <tau11@protonmail.ch>
2023-07-18 18:55:05 +0530

    some improvements

--- src/lib/components/forms/auth.svelte
+++ src/lib/components/forms/auth.svelte
@@ -4,7 +4,7 @@
 	import type { AnyZodObject } from 'zod'
 
 	export let data: string | Validation<AnyZodObject> | null | undefined
-
+  
 	const { form, errors, constraints, enhance } = superForm(data)
 </script>
 
@@ -39,11 +39,11 @@
 				bind:value={$form.password}
 				{...$constraints.password}
 			/>
-		</label>
+		</label> 
 		{#if $errors.password}
 			<span class="text-red-400">{$errors.password}</span>
 		{/if}
-
+      
 		<button class="btn variant-filled mt-4" type="submit">Submit</button>
 	</form>
 </div>
--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import AuthForm from '$lib/components/forms/auth.svelte'
-
 	export let data
 </script>
 
 <div class="mx-auto mt-32 max-w-sm">
 	<h1>Register</h1>
-	<AuthForm data={data.form} />
+	<AuthForm mail=true data={data.form} />
 </div>
